f64c1ccf127d84987a2d17ff4579149e336887c3
amaltr42 <amelia.travers@gmail.com>
2020-11-29 11:39:08 +1100

    added global styles and reset

--- linktree/src/pages/index.js
+++ linktree/src/pages/index.js
@@ -4,6 +4,8 @@ import UserHandle from "../components/UserHandle";
 import ClassicLink from "../components/ClassicLink";
 import ShowsLink from "../components/ShowsLink";
 import MusicStyle from "../components/MusicStyle";
+import '../styles/reset.css';
+import '../styles/global.css';
 
 export default function Home() {
   return (
