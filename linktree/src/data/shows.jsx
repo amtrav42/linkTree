8cddddc5a616aa0628d3ebc0649581d122bea2ac
amaltr42 <amelia.travers@gmail.com>
2020-11-29 15:18:35 +1100

    added show data

---
+++ linktree/src/data/shows.jsx
@@ -0,0 +1,26 @@
+export default [
+  {
+    date: '1/12/20',
+    href: 'https://podcasts.apple.com/au/podcast/avid-research/id1527745431',
+    location: 'Melbourne',
+    isAvailable: true,
+  },
+  {
+    date: '2/12/20',
+    href: 'https://podcasts.apple.com/au/podcast/avid-research/id1527745431',
+    location: 'Sydney',
+    isAvailable: true,
+  },
+  {
+    date: '3/12/20',
+    href: 'https://podcasts.apple.com/au/podcast/avid-research/id1527745431',
+    location: 'Brisbane',
+    isAvailable: false,
+  },
+  {
+    date: '4/12/20',
+    href: 'https://podcasts.apple.com/au/podcast/avid-research/id1527745431',
+    location: 'Hobart',
+    isAvailable: true,
+  },
+];
