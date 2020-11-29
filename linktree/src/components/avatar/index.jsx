91b7ea463e1bd6f2188208694d34dbeca1a4a2eb
amaltr42 <amelia.travers@gmail.com>
2020-11-29 16:22:54 +1100

    added todo comments

--- linktree/src/components/avatar/index.jsx
+++ linktree/src/components/avatar/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Avatar() {
+  // to do add in an image Component
+  // possibly with CSS styling around the image to make it clipped to a circle
   return <div>I am an Avatar Image</div>;
 }
--- linktree/src/components/userHandle/index.jsx
+++ linktree/src/components/userHandle/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export default function UserHandle() {
+  // todo add in padding, content as prop and themeable colour
   return <div>Avid Research</div>;
 }
