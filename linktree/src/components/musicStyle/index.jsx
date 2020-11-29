7b2f66394cabfbd3dddacda0d7b2275be1ee575b
amaltr42 <amelia.travers@gmail.com>
2020-11-29 16:30:43 +1100

    added music style todos

--- linktree/src/components/musicStyle/index.jsx
+++ linktree/src/components/musicStyle/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import ClassicLink from '../classicLink';
 
 export default function MusicStyle() {
-  return <div>I am a Music Style Link</div>;
+  return (
+  // ToDo - Similar state based situation as the shows link for open
+  // Would again need to generate from a list of providers including their icons, URLs Etc
+  // Would need to embed an audio player which was compatable with all relevant APIs
+  // Would probably start with react player as per Avid Research Website, quite personaliseable
+  // Would need multiple on click handlers for each interactive element
+  // Would probably try and break those out into different components
+
+    <ClassicLink>
+      Music Style
+    </ClassicLink>
+  );
 }
